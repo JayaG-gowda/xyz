e3ac69289929efe08fcf8191ac0a46d6dc1dc1d0
Jayanna G <119968609+JayaG-gowda@users.noreply.github.com>
2025-12-18 17:55:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ class Config(object):
 
     #BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "1865366440:AAHdy8hYjQZwMWzihN8m5B7Ezlr2bV8OQIg") #testing bot 1 (mj btn)
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "1865366440:AAEzQuAZcw5t8F9JdBgSqe5aHMHOGD7eTfg") #testing bot 1 (mj btn)
     #BOT_TOKEN = os.environ.get("BOT_TOKEN", "5872747581:AAHVN_3OP9uffBCKdesYZXFigzVuRYWLYOY") #testing bot 2 (url_v3)
     
     API_ID = int(os.environ.get("API_ID", "3393749"))
